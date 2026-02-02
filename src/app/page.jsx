6a2610fcf9b41e4981f7279225e635f4c57b51c1
Igor Matsenko <aikanaro@live.ru>
2026-02-01 21:06:56 -0500

    Change logo imports to progressive-3.svg
    Updated logo imports to use progressive-3.svg for both Airbnb and Planetaria logos.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,9 @@ import {
   LinkedInIcon,
   XIcon,
 } from '@/components/SocialIcons'
-import logoAirbnb from '@/images/logos/airbnb.svg'
+import logoAirbnb from '@/images/logos/progressive-3.svg'
 // import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoPlanetaria from '@/images/logos/progressive-3.svg'
 // import logoStarbucks from '@/images/logos/starbucks.svg'
 // import image1 from '@/images/photos/image-1.jpg'
 // import image2 from '@/images/photos/image-2.jpg'
